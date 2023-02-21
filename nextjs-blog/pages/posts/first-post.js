2d96d300a9dd4c374240204ebb9cb1b372c16593
Álvaro Esteban Muñoz <aesteban.task@gmail.com>
2023-02-21 19:51:12 +0100

    Assets, Metadata and CSS finished

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,12 +1,27 @@
 import Link from 'next/link';
+import Head from 'next/head';
+import Script from 'next/script';
+import Layout from '../../components/layout';
 
 export default function FirstPost() {
     return (
-        <>
+        <Layout>
+            <Head>
+                <title>First Post</title>
+            </Head>
+            <Script
+                // Strategy controls when to load the third-party script
+                // onLoad runs JS code inmediately after loading the script
+                src="https://connect.facebook.net/en_US/sdk.js"
+                strategy='lazyOnload'
+                onLoad={() =>
+                    console.log('Script loaded correctly, windows.FB has been populated')
+                }
+            />
             <h1>First Post</h1>
             <h2>
                 <Link href="/">Back to home</Link>
             </h2>
-        </>
+        </Layout>
     );
 }
